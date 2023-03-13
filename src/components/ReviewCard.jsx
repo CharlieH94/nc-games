a0f16d84f64c52d83daab42a66aeefd03ba97a8d
CharlieH94 <94218945+CharlieH94@users.noreply.github.com>
2023-03-13 18:16:44 +0000

    05 view individual review - responsive

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,15 +1,19 @@
-const ReviewCard = ({review : {title, designer, owner, review_img_url, category, created_at, votes}}) => {
+import { Link } from "react-router-dom";
+
+const ReviewCard = ({ review: {review_id, title, designer, owner, review_img_url, category, created_at, votes } }) => {
     return (
-        <section className='review'>
-            <figure>
-                <img src={review_img_url} alt={title} className='review__img'/>
-            </figure>
-            <figcaption>
-                <h2 className='review__title'>{title}</h2>
-                <p>by {owner}</p>
-                <p>Votes: {votes}</p>
-            </figcaption>
-        </section>
+        <Link to={`/reviews/${review_id}`} style={{ textDecoration:'none' }}>
+            <section className='review'>
+                <figure>
+                    <img src={review_img_url} alt={title} className='review__img'/>
+                </figure>
+                <figcaption>
+                    <h2 className='review__title'>{title}</h2>
+                    <p>by {owner}</p>
+                    <p>Votes: {votes}</p>
+                </figcaption>
+            </section>
+        </Link>
     )
 }
 
--- src/components/SingleReview.jsx
+++ src/components/SingleReview.jsx
@@ -1,12 +1,47 @@
-import { useParams } from "react-router";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getReviews } from "../utils/api";
+import Nav from "./Nav";
 
 const SingleReview = () => {
-    const {review_id} = useParams;
-    console.log(review_id)
+    const [review, setReview] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+    const { review_id } = useParams();
+    const { review_img_url, title, owner, category, review_body, votes, comment_count, created_at } = review;
 
+    useEffect(() => {
+        setIsLoading(true);
+        getReviews(+review_id).then(reviewData => {
+            setReview(reviewData);
+            setIsLoading(false);
+        })
+    }, [review_id])
 
-    return (
-        <p>hello there</p>
+    return isLoading ? (
+        <div>
+            <Nav />
+            <p>Loading Review...</p>
+        </div>
+    )
+        :  (
+            <>
+                <Nav />
+                <section className='single-review'> 
+                    <h2 className='single-review__title'>{title}</h2>
+                    <figure>
+                        <img src={review_img_url} alt={title} className='single-review__img'/>
+                    </figure>
+                    <figcaption>
+                        <h3 className='single-review__owner'>by {owner}</h3>
+                        <h4><em>{category}</em></h4>
+                        <p className='single-review__body'>{review_body}</p>
+                    </figcaption>
+                    <div className='popularity-stats'>
+                            <p>Comment Count: {comment_count}</p>
+                            <p>Votes: {votes}</p>
+                        </div>
+                </section>
+            </>     
     )
 };
 
