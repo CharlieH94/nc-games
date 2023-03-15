5e2c48cce3e605ebcb4a8fbbb4fd08247bc8cf38
CharlieH94 <94218945+CharlieH94@users.noreply.github.com>
2023-03-15 13:46:55 +0000

    10 ongoing - option choice currently reflected in state

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -8,21 +8,45 @@ const Reviews = () => {
     const [reviews, setReviews] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [searchParams, setSearchParams] = useSearchParams();
+    const [sortByCommentCount, setSortByCommentCount] = useState(false);
+    const [sortBySelection, setSortBySelection] = useState(null);
+    const [orderSelection, setOrderSelection] = useState(null);
 
-    const sort_by = searchParams.get('sort_by')
-    // const category = searchParams.get('category')
-
-    // const params = {sort_by, category};
-    
     const userSelectedCategory = searchParams.get('category')
+    const userSelectedSortBy = searchParams.get('sort_by')
+    const userSelectedOrder = searchParams.get('order')
 
+    const params = {category: userSelectedCategory, sort_by: userSelectedSortBy, order: userSelectedOrder};
+    
     useEffect(() => {
         setIsLoading(true);
-        getReviews(null, userSelectedCategory).then(reviewData => {
+        getReviews(null, params).then(reviewData => {
             setReviews(reviewData)
             setIsLoading(false);
         } );
     }, [userSelectedCategory])
+
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    const onChange = (event) => {
+        const query = event.target.id;
+        const option = event.target.value;
+        console.log(option);
+        
+        if (query === 'sort_by') {
+            if (option === 'Comment Count') setSortByCommentCount(true);
+            else if (option === 'Date') setSortBySelection('');
+            else if (option === 'Votes') setSortBySelection('');
+        }
+        
+        if (query === 'order-by') {
+            if (option === 'Ascending') return;
+            else if (option === 'Descending') return;
+        }
+    }
 
     return isLoading ? (
         <>
@@ -32,7 +56,26 @@ const Reviews = () => {
         : (
         <main>
             <Nav />
-            <ul className='reviews-list'>
+                <ul className='reviews-list'>
+                <form id='organise-input' onSubmit={onSubmit}>
+                <div id='sort-input'>
+                    <label>Sort By: </label>
+                    <select name="" id='sort-by' onChange={onChange} defaultValue="default">
+                        <option value="default" key="no-select" disabled>Select sort option</option>
+                        <option key='date'>Date</option>
+                        <option>Votes</option>
+                        <option>Comment Count</option>
+                    </select>
+                </div>
+                    <div id='order-input'>
+                    <label>Order: </label>
+                    <select name="" id='order-by' onChange={onChange} defaultValue="default">
+                        <option value="default" key="no-select" disabled>Select order</option>
+                        <option>Ascending</option>
+                        <option>Descending</option>
+                    </select>
+                </div>
+            </form>
                 {reviews.map(review => {
                     return <li key={review.title}><ReviewCard review={review} /></li>
                 })}
--- src/utils/api.js
+++ src/utils/api.js
@@ -4,16 +4,12 @@ const ncGamesAPI = axios.create({
     baseURL: "https://be-nc-games-project.onrender.com/api",
   });
   
-  export const getReviews = (review_id, category) => {
+  export const getReviews = (review_id, params) => {
     let path = '/reviews';
 
     if (review_id) path += `/${review_id}`;
 
-    return ncGamesAPI.get(path, {
-      params: {
-        category: category
-      }
-    }).then((response) => {
+    return ncGamesAPI.get(path, {params}).then((response) => {
       return review_id ? response.data.review : response.data.reviews;
   });
 };
